8d7ed3350ab2573d22b3a805e078b7772f0541e4
Kevin <ckevin@Albina-Clements-MacBook-Pro.local>
2014-07-12 21:20:36 +0900

    Code splitted

--- main.rs
+++ main.rs
@@ -7,55 +7,17 @@ extern crate url;
 extern crate sqlite3;
 
 use std::os;
-use std::io::{IoResult, TcpStream, File};
 use url::Url;
 use sqlite3::{SqliteResult};
 
-fn update_desktop_db(path_to_img: &Path) -> SqliteResult<()> {
-  let path_to_db = os::homedir().unwrap().join_many(["Library", "Application Support", "Dock", "desktoppicture.db"]);
-  let db = try!(sqlite3::open(path_to_db.as_str().unwrap()));
-  let query = format!("UPDATE data SET value='{}';", path_to_img.as_str().unwrap());
-  try!(db.exec(query.as_slice()));
-  Ok(())
-}
+mod io;
 
-fn without_headers(vec: &Vec<u8>) -> Box<Vec<u8>> {
-  let mut new_vec = box Vec::new();
-  let mut acc = 0u;
-  for b in vec.iter() {
-    if acc == 4 {
-      new_vec.push(*b);
-    } else {
-      if *b == b'\n' || *b == b'\r' { acc += 1 } else { acc = 0 }
-    }
-  }
-  new_vec
-}
-
-fn download_file(url: &Url) -> IoResult<Box<Path>> {
-  let mut socket = try!(TcpStream::connect(url.host.as_slice(), 80));
-  let req = format!(
-    "GET {:s} HTTP/1.1\r\nHost: {:s}\r\nAccept: */*\r\n\r\n",
-    url.path.path.as_slice(), url.host
-  );
-  try!(socket.write(req.as_bytes()));
-  let res = try!(socket.read_to_end());
-
-  let filepath = box os::homedir().unwrap().join_many(["Pictures", "foobar"]);
-  //let filepath = box os::tmpdir().join(Path::new("foobar"));
-  let mut file = File::create(filepath);
-  let res_without_headers = without_headers(&res);
-  try!(file.write(res_without_headers.as_slice()));
-  Ok(filepath)
-}
-
-//use try!
 fn exec(arg: &String) -> SqliteResult<()> {
   let re = regex!(r"http://|https://");
   let path =
     if re.is_match(arg.as_slice()) {
       match Url::parse(arg.as_slice()) {
-        Ok(url) => match download_file(&url) {
+        Ok(url) => match io::download_file(&url) {
           Ok(p) => p,
           Err(e) => fail!("Io Error: {}", e)
         },
@@ -64,7 +26,7 @@ fn exec(arg: &String) -> SqliteResult<()> {
     } else {
       box Path::new(arg.as_slice())
     };
-  Ok(try!(update_desktop_db(path)))
+  Ok(try!(io::update_desktop_db(path)))
 }
 
 #[allow(unused_must_use)]
