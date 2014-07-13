9e85ef9fef36768a1f2d76611fa671abeef1d425
Kevin <ckevin@Albina-Clements-MacBook-Pro.local>
2014-07-13 14:08:35 +0900

    Help message displayed

--- main.rs
+++ main.rs
@@ -9,7 +9,7 @@ extern crate sqlite3;
 
 use std::os;
 use url::Url;
-use getopts::{Matches, optflag, getopts};
+use getopts::{OptGroup, optflag, getopts};
 use sqlite3::{SqliteResult};
 
 mod io;
@@ -31,18 +31,33 @@ fn exec(arg: &String) -> SqliteResult<()> {
     Ok(try!(io::update_desktop_db(&path)))
 }
 
-fn extract_args() -> (Matches, Vec<String>) {
-    let args = os::args();
-    let opts = [ optflag("k", "kill", "Killall Dock") ];
-    match getopts(args.tail(), opts) {
-        Ok(m) => (m.clone(), m.free),
-        Err(f) => fail!(f.to_string())
+fn print_usage(program: &String, opts: &[OptGroup]) {
+    println!("Usage: {} [/path/to/picture|url]", program);
+    for opt in opts.iter() {
+        println!("{} {}\t{}", opt.short_name, opt.long_name, opt.desc);
     }
 }
 
 #[allow(unused_must_use)]
 fn main() {
-    let (matches, args) = extract_args();
+    let raw_args = os::args();
+    let program = raw_args.get(0).clone();
+
+    let opts = [
+        optflag("k", "kill", "refresh the ui with `killall dock`"),
+        optflag("h", "help", "print this help menu")
+    ];
+
+    let (matches, args) = match getopts(raw_args.tail(), opts) {
+        Ok(m) => (m.clone(), m.free),
+        Err(f) => fail!(f.to_string())
+    };
+
+    if matches.opt_present("h") || args.len() == 0 {
+        print_usage(&program, opts);
+        return;
+    }
+
     if args.len() > 0 {
         exec(args.get(0));
         if matches.opt_present("k") {
