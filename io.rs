12376ab17084b36cd813f3abb6f025b0023b8d88
Kevin <ckevin@Albina-Clements-MacBook-Pro.local>
2014-07-13 13:46:18 +0900

    Better directory and name for downloaded pictures

--- io.rs
+++ io.rs
@@ -1,8 +1,10 @@
 extern crate sqlite3;
+extern crate time;
 
 use std::os;
 use url::Url;
-use std::io::{IoResult, TcpStream, File, Command};
+use std::io::{IoResult, TcpStream, File, Command, UserRWX};
+use std::io::fs;
 use sqlite3::{SqliteResult};
 
 pub fn kill_dock() -> IoResult<()> {
@@ -11,7 +13,6 @@ pub fn kill_dock() -> IoResult<()> {
 }
 
 pub fn update_desktop_db(path_to_img: &Path) -> SqliteResult<()> {
-    //unwrap, unwrap, unwrap... consider using match
     let path_to_db = os::homedir().unwrap().join_many(["Library", "Application Support", "Dock", "desktoppicture.db"]);
     let db = try!(sqlite3::open(path_to_db.as_str().unwrap()));
     let query = format!("UPDATE data SET value='{}';", path_to_img.as_str().unwrap());
@@ -19,6 +20,7 @@ pub fn update_desktop_db(path_to_img: &Path) -> SqliteResult<()> {
     Ok(())
 }
 
+//Split this function
 pub fn download_file(url: &Url) -> IoResult<Path> {
     let mut socket = try!(TcpStream::connect(url.host.as_slice(), 80));
     let req = format!(
@@ -28,9 +30,12 @@ pub fn download_file(url: &Url) -> IoResult<Path> {
     try!(socket.write(req.as_bytes()));
     let res = try!(socket.read_to_end());
 
-    //Find the good folder and picture name here:
-    let filepath = os::homedir().unwrap().join_many(["Pictures", "wallpaper"]);
-    //let filepath = os::tmpdir().join("wallpaper");
+    let dirpath = os::homedir().unwrap().join_many(["Library", "wallpaper"]);
+    if !dirpath.exists() {
+        try!(fs::mkdir(&dirpath, UserRWX));
+    }
+    let ts = time::now().to_timespec();
+    let filepath = dirpath.join(format!("wall-{:d}-{:d}", ts.sec, ts.nsec));
     let mut file = File::create(&filepath);
     let res_without_headers = without_headers(&res);
     try!(file.write(res_without_headers.as_slice()));
